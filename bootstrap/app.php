f9073bceaf1f8b75aaf4523df0027afe2893ccdf
kp117909 <kpolak491@gmail.com>
2025-09-17 11:36:08 +0200

    Fix HTTPS mixed content error for Railway deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
+        $middleware->trustProxies(at: '*');
 
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
